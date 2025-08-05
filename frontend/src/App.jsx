adddd71fdf2e04e9bae992ff28427ceee1b8bae4
BhaveshGadling77 <bhaveshgadling2025@gmail.com>
2025-08-05 22:51:58 +0530

    Footer and Feature Div Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Navbar from './components/Navbar'
-import Tagline from './components/Tagline'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Tagline from "./components/Tagline";
+import Feature from "./components/feature";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <Navbar />
-    <Tagline />
-    
+      <Navbar />
+      <Tagline />
+     <div className="space-x-9 justify-center align-middle text-center">
+      <Feature />
+      <Feature />
+      <Feature />
+      </div> 
     </>
-
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/Tagline.jsx
+++ frontend/src/components/Tagline.jsx
@@ -4,7 +4,7 @@ function Tagline() {
         <>
             <div className="py-96 text-center font-cedarville">
                 <p className="text-7xl" id="tagline" >Saarthi</p>
-                <p className="mt-4" >A partner in hygiene and convenience</p>
+                <p className="mt-4" >स्वच्छता और सुविधा का साथी</p>
             </div>
         </>
     )
