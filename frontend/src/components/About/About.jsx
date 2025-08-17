dfa9ba9d4a159de080bafb5669b642076eebe487
BhaveshGadling77 <bhaveshgadling2025@gmail.com>
2025-08-18 00:20:21 +0530

    Started Making About Page.

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -1,7 +1,59 @@
+import "./index.css";
+import Section from "./sectionComponent";
 function About() {
+    let data = [
+        {
+            name:"Bhavesh Gadling",
+            job:"Frontend Developer",
+            workDone:"Building user-friendly UI, integrating Backend and interfaces for pilgrims."
+        },
+        {
+            name:"Aditi Bhise",
+            job:"API integration & System Glue",
+            workDone:"Connecting Backend Data with Frontend features and all Api related work."
+        },
+        {
+            name:"Sangram Lohkare",
+            job:"Backend And Data Handling",
+            workDone:"Managing data Storage, retrieal and ensuring realible access during user load."
+        }]
     return (
-        <div className="min-h-screen flex flex-col">
-            <div className="mt-36">this will be the About components</div>
+        <div className="min-h-screen flex flex-col bg-[#FAF7EB]">
+            <div className="mt-36 text-center text-[#2F3025] text-3xl"
+                id="about">
+                <p className="text-wrap text-5xl" id="about">
+                    About
+                </p>
+            </div>
+            <div className="flex justify-center mt-4">
+                <div className="text-center w-[380px] align-middle justify-center">
+                    <p>
+                        Saarthi guides Pilgrims through health, safety and
+                        hygiene During Simhasta 2028.
+                    </p>
+                </div>
+            </div>
+            { /* About Section*/}
+            <div className="text-center mt-12 text-[#2F3025]" id="about">
+                <p className="text-wrap text-2xl">Our Story</p>
+            </div>
+            <div className="flex justify-center mt-4">
+                <div className="text-center w-[350px] align-middle justify-center">
+                    <p>
+                        In Ancient texts, Saarthi (charioteer) is the guide who
+                        helps navigate safely, Similarly, our app Saarthi guides
+                        millions of pilgrims through health, safety and hygiene
+                        during Simhasta 2028.
+                    </p>
+                </div>
+            </div>
+            <div className="text-center mt-7 text-[#2F3025]" id="about">
+                <p className="text-wrap text-2xl">Meet The Team</p>
+            </div>
+            {/* this will be the */}
+            <div className="">
+                {data.map(data => <Section data={data}/>)}
+            </div>
         </div>
     );
 }
---
+++ frontend/src/components/About/sectionComponent.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Section(props) {
+console.log(props)
+    return (
+        <div className="text-center">
+            <div className="font-bold">Bhavesh Gadling</div>
+        </div>
+    )
+}
+export default Section;
