f043514e73c9fab079225f137d9ec152151d3c44
BhaveshGadling77 <bhaveshgadling2025@gmail.com>
2025-08-21 22:32:01 +0530

    Trying to add Firebase.

---
+++ frontend/src/config/firebase.jsx
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAT7F5LUez5-9NmCpOkZRR8BMGxoqPJA_Q",
+  authDomain: "saarthi-d8d0f.firebaseapp.com",
+  projectId: "saarthi-d8d0f",
+  storageBucket: "saarthi-d8d0f.firebasestorage.app",
+  messagingSenderId: "608733209149",
+  appId: "1:608733209149:web:3f7a69796a26e9c1374054",
+  measurementId: "G-3Z0SZWK0H6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
