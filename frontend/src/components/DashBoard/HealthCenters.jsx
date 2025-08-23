b1d96263c5b52260387cfded7b90c3836d475502
BhaveshGadling77 <bhaveshgadling2025@gmail.com>
2025-08-24 00:04:38 +0530

    Trying to connect the server

--- frontend/src/components/DashBoard/HealthCenters.jsx
+++ frontend/src/components/DashBoard/HealthCenters.jsx
@@ -1,3 +1,14 @@
+import React from "react";
+import useFetch from "../UseFetch";
+
+// const {data, isPending, Erro} = useFetch('http://127.0.0.1:8000/washrooms')
+
+// console.log(data)
+// fetch('http://127.0.0.1:8000/feedback')
+//     .then((res) => res.json())
+//     .then((data) => {
+//         console.log(data)
+//     })
 function HealthCenters() {
     return (
         <>
