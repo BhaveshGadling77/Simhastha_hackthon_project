0a5fb138da28b604f2a8ba5f081fe6b38e6778bb
BhaveshGadling77 <bhaveshgadling2025@gmail.com>
2025-08-26 23:50:05 +0530

    trying to add context api.

--- frontend/src/context/data.js
+++ frontend/src/context/data.js
@@ -4,7 +4,9 @@ export const dataContext = createContext({
     data:null
 })
 
+
 export const dataProvide = data.Provider;
+
 export default function useData() {
     return useContext(dataContext)
 }
